109cd305def0e31b72e0ec8ff16c30d5f5bae2f6
Diego Ferigo <diego.ferigo@iit.it>
2021-07-20 12:07:06 +0200

    Add docker resources

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,33 @@
-FROM alpine:3.10
+FROM ubuntu:focal
 
-COPY LICENSE README.md /
+ARG DEBIAN_FRONTEND=noninteractive
+RUN apt-get update &&\
+    apt-get install -y --no-install-recommends \
+        git \
+        wget \
+        gnupg2 \
+        software-properties-common \
+    &&\
+    rm -rf /var/lib/apt/lists/*
+
+RUN wget -nv -O - http://apt.llvm.org/llvm-snapshot.gpg.key | apt-key add - &&\
+    apt-add-repository -y "deb http://apt.llvm.org/`lsb_release -cs`/ llvm-toolchain-`lsb_release -cs` main" &&\
+    apt-get install -y --no-install-recommends \
+        clang-format-6.0 \
+        clang-format-7 \
+        clang-format-8 \
+        clang-format-9 \
+        clang-format-10 \
+        clang-format-11 \
+        clang-format-12 \
+        clang-format-13 \
+    &&\
+    rm -rf /var/lib/apt/lists/*
 
 COPY entrypoint.sh /entrypoint.sh
+RUN chmod 755 /entrypoint.sh
+
+COPY check_clang_format_compliance.sh /usr/bin/check_clang_format_compliance
+RUN chmod 755 /usr/bin/check_clang_format_compliance
 
 ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,52 @@
-#!/bin/sh -l
+#!/bin/bash -l
 
-echo "hello $1"
+if [[ $DEBUG_MODE -eq 1 ]]; then
+  echo
+  echo "     +INPUT_VERSION=$INPUT_VERSION"
+  echo "     +INPUT_DIRECTORY=$INPUT_DIRECTORY"
+  echo "     +INPUT_PATTERN=$INPUT_PATTERN"
+  echo "     +INPUT_STYLE=$INPUT_STYLE"
+  echo "     +INPUT_FALLBACK=$INPUT_FALLBACK"
+  echo "     +INPUT_FAIL_ON_ERROR=$INPUT_FAIL_ON_ERROR"
+  echo
+fi
+
+# Split the file pattern string obtaining an array
+readarray -t INPUT_PATTERN_ARRAY < <(echo -e $INPUT_PATTERN)
+
+# Create multiple -p PATTERN options allowing to use in yaml:
+# pattern: |
+#   *.h
+#   *.hh
+#   *.cpp
+for p in ${INPUT_PATTERN_ARRAY[@]} ; do
+  pattern_cmd_option="$pattern_cmd_option -p $p "
+done
+
+# Call the style check script
+check_clang_format_compliance \
+  -v "$INPUT_VERSION" \
+  -d "$INPUT_DIRECTORY" \
+  -s "$INPUT_STYLE" \
+  -f "$INPUT_FALLBACK" \
+  $pattern_cmd_option
+
+# Catch the exit status
+exit_status=$?
+
+# Get the diff and fix escape special characters
+# https://github.community/t/set-output-truncates-multiline-strings/16852
+diff=$(</tmp/style_diff.txt)
+diff="${diff//'%'/'%25'}"
+diff="${diff//$'\n'/'%0A'}"
+diff="${diff//$'\r'/'%0D'}"
+
+# Store the diff in an output variable
+echo "::set-output name=diff::$diff"
+
+# Propagate the exit status
+if [[ $INPUT_FAIL_ON_ERROR -eq 1 ]] ; then
+  exit $exit_status
+else
+  exit 0
+fi
